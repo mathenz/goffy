a712bdfd2ee25fa34fbe55746a96d7ea887635dd
mathenz <145191341+mathenz@users.noreply.github.com>
2023-11-22 01:11:38 -0500

    get album info too

--- spotify.go
+++ spotify.go
@@ -1,9 +1,3 @@
-/*
-This is an unofficial (rustical and intentionally feature-limited) version of the Spotify Web API.
-The reason of not using the official API is because it would be a waste of time
-because this program has no features related to users' private data
-*/
-
 package main
 
 import (
@@ -19,7 +13,8 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-type PlaylistEndpoint struct {
+/* for playlists and albums */
+type ResourceEndpoint struct {
 	Limit, Offset, TotalCount, Requests int64
 }
 
@@ -31,8 +26,10 @@ const (
 	tokenEndpoint       = "https://open.spotify.com/get_access_token?reason=transport&productType=web-player"
 	trackInitialPath    = "https://api-partner.spotify.com/pathfinder/v1/query?operationName=getTrack&variables="
 	playlistInitialPath = "https://api-partner.spotify.com/pathfinder/v1/query?operationName=fetchPlaylist&variables="
+	albumInitialPath    = "https://api-partner.spotify.com/pathfinder/v1/query?operationName=getAlbum&variables="
 	trackEndPath        = `{"persistedQuery":{"version":1,"sha256Hash":"e101aead6d78faa11d75bec5e36385a07b2f1c4a0420932d374d89ee17c70dd6"}}`
 	playlistEndPath     = `{"persistedQuery":{"version":1,"sha256Hash":"b39f62e9b566aa849b1780927de1450f47e02c54abf1e66e513f96e849591e41"}}`
+	albumEndPath        = `{"persistedQuery":{"version":1,"sha256Hash":"46ae954ef2d2fe7732b4b2b4022157b2e18b7ea84f70591ceb164e4de1b5d5d3"}}`
 )
 
 func accessToken() (string, error) {
@@ -92,7 +89,7 @@ func isValidPattern(url, pattern string) bool {
 func TrackInfo(url string) (*Track, error) {
 	trackPattern := `^https:\/\/open\.spotify\.com\/track\/[a-zA-Z0-9]{22}\?si=[a-zA-Z0-9]{16}$`
 	if !isValidPattern(url, trackPattern) {
-		return nil, errors.New("invalid track")
+		return nil, errors.New("invalid track url")
 	}
 
 	id := getID(url)
@@ -120,57 +117,94 @@ func TrackInfo(url string) (*Track, error) {
 func PlaylistInfo(url string) ([]Track, error) {
 	playlistPattern := `^https:\/\/open\.spotify\.com\/playlist\/[a-zA-Z0-9]{22}\?si=[a-zA-Z0-9]{16}$`
 	if !isValidPattern(url, playlistPattern) {
-		return nil, errors.New("invalid playlist")
+		return nil, errors.New("invalid playlist url")
 	}
 
-	id := getID(url)
-	pConf := PlaylistEndpoint{Limit: 400}
-	endpointQuery := EncodeParam(fmt.Sprintf(`{"uri":"spotify:playlist:%s","offset":%d,"limit":%d}`, id, pConf.Offset, pConf.Limit))
-	endpoint := playlistInitialPath + endpointQuery + "&extensions=" + EncodeParam(playlistEndPath)
-
-	statusCode, jsonResponse, err := request(endpoint)
+	totalCount := "data.playlistV2.content.totalCount"
+	itemsArray := "data.playlistV2.content.items"
+	tracks, err := resourceInfo(url, "playlist", totalCount, itemsArray)
 	if err != nil {
-		return nil, fmt.Errorf("error getting the tracks of the playlist: %w", err)
+		return nil, err
 	}
 
-	if statusCode != 200 {
-		return nil, fmt.Errorf("received non-200 status code: %d", statusCode)
+	return tracks, nil
+}
+
+func AlbumInfo(url string) ([]Track, error) {
+	albumPattern := `^https:\/\/open\.spotify\.com\/album\/[a-zA-Z0-9-]{22}\?si=[a-zA-Z0-9_-]{22}$`
+	if !isValidPattern(url, albumPattern) {
+		return nil, errors.New("invalid album url")
 	}
 
-	pConf.TotalCount = gjson.Get(jsonResponse, "data.playlistV2.content.totalCount").Int()
-	if pConf.TotalCount < 1 {
-		return nil, errors.New("playlist is empty")
+	totalCount := "data.albumUnion.discs.items.0.tracks.totalCount"
+	itemsArray := "data.albumUnion.discs.items"
+	tracks, err := resourceInfo(url, "album", totalCount, itemsArray)
+	if err != nil {
+		return nil, err
 	}
 
-	name := gjson.Get(jsonResponse, "data.playlistV2.name").String()
-	fmt.Printf("Collecting tracks from the playlist '%s'...\n", name)
-	time.Sleep(2 * time.Second)
+	return tracks, nil
+}
 
-	pConf.Requests = int64(math.Ceil(float64(pConf.TotalCount) / float64(pConf.Limit)))
+/* returns playlist/album slice of tracks */
+func resourceInfo(url, resourceType, totalCount, itemList string) ([]Track, error) {
+	id := getID(url)
+	eConf := ResourceEndpoint{Limit: 400, Offset: 0}
+	jsonResponse, err := jsonList(resourceType, id, eConf.Offset, eConf.Limit)
+	if err != nil {
+		return nil, err
+	}
 
+	eConf.TotalCount = gjson.Get(jsonResponse, totalCount).Int()
+
+	if eConf.TotalCount < 1 {
+		return nil, errors.New("hum, there are no tracks")
+	}
+
+	name := map[bool]string{true: gjson.Get(jsonResponse, "data.playlistV2.name").String(), false: gjson.Get(jsonResponse, "data.albumUnion.name").String()}[resourceType == "playlist"]
+	fmt.Printf("Collecting tracks from '%s'...\n", name)
+	time.Sleep(1 * time.Second)
+
+	eConf.Requests = int64(math.Ceil(float64(eConf.TotalCount) / float64(eConf.Limit))) /* total of requests */
 	var tracks []Track
-	tracks = append(tracks, processPlaylist(jsonResponse)...)
+	tracks = append(tracks, proccessItems(jsonResponse, resourceType)...)
 
-	for i := 1; i < int(pConf.Requests); i++ {
-		pConf.pagination()
-		endpointQuery = EncodeParam(fmt.Sprintf(`{"uri":"spotify:playlist:%s","offset":%d,"limit":%d}`, id, pConf.Offset, pConf.Limit))
-		endpoint = playlistInitialPath + endpointQuery + "&extensions=" + EncodeParam(playlistEndPath)
+	for i := 1; i < int(eConf.Requests); i++ {
+		eConf.pagination()
 
-		statusCode, jsonResponse, err = request(endpoint)
+		jsonResponse, err := jsonList(resourceType, id, eConf.Offset, eConf.Limit)
 		if err != nil {
-			return nil, fmt.Errorf("error getting the tracks of the playlist: %w", err)
+			return nil, err
 		}
-
-		if statusCode != 200 {
-			return nil, fmt.Errorf("received non-200 status code: %d", statusCode)
-		}
-
-		tracks = append(tracks, processPlaylist(jsonResponse)...)
+		tracks = append(tracks, proccessItems(jsonResponse, resourceType)...)
 	}
 
 	fmt.Println("Tracks collected:", len(tracks))
-
 	return tracks, nil
+}
+
+/* gets JSON respond from playlist/album endpoints */
+func jsonList(resourceType, id string, offset, limit int64) (string, error) {
+	var endpointQuery string
+	var endpoint string
+	if resourceType == "playlist" {
+		endpointQuery = EncodeParam(fmt.Sprintf(`{"uri":"spotify:playlist:%s","offset":%d,"limit":%d}`, id, offset, limit))
+		endpoint = playlistInitialPath + endpointQuery + "&extensions=" + EncodeParam(playlistEndPath)
+	} else {
+		endpointQuery = EncodeParam(fmt.Sprintf(`{"uri":"spotify:album:%s","locale":"","offset":%d,"limit":%d}`, id, offset, limit))
+		endpoint = albumInitialPath + endpointQuery + "&extensions=" + EncodeParam(albumEndPath)
+	}
+
+	statusCode, jsonResponse, err := request(endpoint)
+	if err != nil {
+		return "", fmt.Errorf("error getting tracks: %w", err)
+	}
+
+	if statusCode != 200 {
+		return "", fmt.Errorf("received non-200 status code: %d", statusCode)
+	}
+
+	return jsonResponse, nil
 }
 
 func (t *Track) buildTrack() *Track {
@@ -183,21 +217,27 @@ func (t *Track) buildTrack() *Track {
 	return track
 }
 
-func (pConf *PlaylistEndpoint) pagination() {
-	pConf.Offset = pConf.Offset + pConf.Limit
+func (eConf *ResourceEndpoint) pagination() {
+	eConf.Offset = eConf.Offset + eConf.Limit
 }
 
-/* constructs each Spotify track from JSON body and returns a slice of tracks */
-func processPlaylist(jsonResponse string) []Track {
-	items := gjson.Get(jsonResponse, "data.playlistV2.content.items").Array()
+/* constructs each Spotify track from JSON body (album/playlist) and returns a slice of tracks */
+func proccessItems(jsonResponse, resourceType string) []Track {
+	itemList := map[bool]string{true: "data.playlistV2.content.items", false: "data.albumUnion.tracks.items"}[resourceType == "playlist"]
+	songTitle := map[bool]string{true: "itemV2.data.name", false: "track.name"}[resourceType == "playlist"]
+	artistName := map[bool]string{true: "itemV2.data.artists.items.0.profile.name", false: "track.artists.items.0.profile.name"}[resourceType == "playlist"]
+	albumName := map[bool]string{true: "itemV2.data.albumOfTrack.name", false: "data.albumUnion.name"}[resourceType == "playlist"]
+
 	var tracks []Track
+	items := gjson.Get(jsonResponse, itemList).Array()
 
 	for _, item := range items {
 		track := &Track{
-			Title:  item.Get("itemV2.data.name").String(),
-			Artist: item.Get("itemV2.data.artists.items.0.profile.name").String(),
-			Album:  item.Get("itemV2.data.albumOfTrack.name").String(),
+			Title:  item.Get(songTitle).String(),
+			Artist: item.Get(artistName).String(),
+			Album:  map[bool]string{true: item.Get(albumName).String(), false: gjson.Get(jsonResponse, albumName).String()}[resourceType == "playlist"],
 		}
+
 		tracks = append(tracks, *track.buildTrack())
 	}
 
