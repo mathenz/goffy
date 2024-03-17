cc8371337786feab9d052a44af6d18f90a369d1a
mathenz <145191341+mathenz@users.noreply.github.com>
2024-03-17 03:23:04 -0500

    rename function calls

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-
+	    
 	"github.com/fatih/color"
 )
 
@@ -24,50 +24,39 @@ var (
 )
 
 func main() {
-	/* get the dir where the program is invoked:
-	the purpose of this is because when user wants to save the music to mobile device
-	it creates a temporary dir on the path where the program is invoked
-	so, if the program is interrumpted or finished, it is necessary delete the temporary dir
-	and the zip file */
-	workingDir, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	tempDir := filepath.Join(workingDir, "YourMusic")
-	zipFile := filepath.Join(workingDir, "YourMusic.zip")
-
-	SetupCloseHandler(tempDir, zipFile)
-
 	flag.StringVar(&trackF, "t", "", "Download a single track. Usage: -t URL")
 	flag.StringVar(&playlistF, "p", "", "Download an entire playlist. Usage: -p URL")
 	flag.StringVar(&albumF, "a", "", "Download an album. Usage: -a URL")
 	flag.StringVar(&fileF, "f", "", "Download multiple tracks from a txt file. Usage: -f /PATH/TO/TXT")
-	flag.StringVar(&desktopF, "d", "", "Specify the path to save the music on your PC. Usage: -d /PATH/TO/MUSIC/FOLDER/")
+	flag.StringVar(&desktopF, "d", "", "Specify the path to save the music locally. Usage: -d /PATH/TO/MUSIC/FOLDER/")
 	flag.BoolVar(&mobileF, "m", false, "Save music on your mobile device. Don't have to specify any path. Usage: -m")
 
-	flag.Usage = func() {
-		fmt.Print("Usage: ")
-		boldWhite.Println("goffy [option] [url] [platform] [/path/to/music/folder/]")
+    flag.Usage = func() {
+    		fmt.Print("Usage: ")
+    		boldWhite.Println("goffy [option] [url] [platform] [/path/to/music/folder/]")
 
-		fmt.Println("If [option] is -f, [url] is /path/to/txt")
-		fmt.Println("If [platform] is -m, [path] is omitted.")
+    		fmt.Println("If [option] is -f, [url] is /path/to/txt")
+    		fmt.Println("If [platform] is -m, [path] is omitted.")
 
-		fmt.Printf("\nOptions:\n")
-		flag.VisitAll(func(f *flag.Flag) {
-			if f.Name != "d" && f.Name != "m" {
-				fmt.Printf("  -%s:	%s\n", f.Name, f.Usage)
-			}
-		})
-		fmt.Printf("\nPlatform:\n")
-		flag.VisitAll(func(f *flag.Flag) {
-			if f.Name == "d" || f.Name == "m" {
-				fmt.Printf("  -%s:	%s\n", f.Name, f.Usage)
-			}
-		})
-	}
-
+    		fmt.Printf("\nOptions:\n")
+    		flag.VisitAll(func(f *flag.Flag) {
+    			if f.Name != "d" && f.Name != "m" {
+    				fmt.Printf("  -%s	%s\n", f.Name, f.Usage)
+    			}
+    		})
+    		fmt.Printf("\nPlatform:\n")
+    		flag.VisitAll(func(f *flag.Flag) {
+    			if f.Name == "d" || f.Name == "m" {
+    				fmt.Printf("  -%s	%s\n", f.Name, f.Usage)
+    			}
+    		})
+    	}
 	flag.Parse()
+	
+	tempDir := filepath.Join(GetCurrentDir(), "YourMusic")
+	zipFile := filepath.Join(GetCurrentDir(), "YourMusic.zip")
+	InterruptHandler(tempDir)
+	InterruptHandler(zipFile)
 
 	ddl := DesktopDownloader{}
 	mdl := MobileDownloader{}
